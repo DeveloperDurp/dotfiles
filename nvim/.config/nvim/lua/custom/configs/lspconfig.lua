8f115aa3fd05dd06615eec76d7ffbe70a65c5058
DeveloperDurp <DeveloperDurp@durp.info>
2023-06-19 19:48:09 -0400

    add nvim

---
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,34 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+}
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
+
+lspconfig.powershell_es.setup({
+  bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services/"
+})
+
+lspconfig.bashls.setup({})
