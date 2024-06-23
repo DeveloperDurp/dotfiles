2b24bdd4a58321c8bdcf446be9c5c44adc03cef1
DeveloperDurp <DeveloperDurp@durp.info>
2024-06-23 09:49:30 -0500

    update

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -2,8 +2,8 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("i", "jk", "<ESC>")
 
 -- Tmux
 map("", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "window left" })
