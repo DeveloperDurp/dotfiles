bc8b5e21a5ff2901af0024599d9810b796e94581
DeveloperDurp <DeveloperDurp@durp.info>
2025-05-17 10:27:17 -0500

    update

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,6 +1,7 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
+    version = "^1.0.0",
     opts = {
       ensure_installed = {
         "gopls",
@@ -23,6 +24,7 @@ return {
       },
     },
   },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
--- .config/shell/export.zsh
+++ .config/shell/export.zsh
@@ -1,4 +1,5 @@
 export ZSH="$HOME/.oh-my-zsh"
+export DEVPOD_DISABLE_TELEMETRY=true
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export POWERSHELL_TELEMETRY_OPTOUT=1 
 export DOTNET_ROOT="/usr/share/dotnet"
@@ -13,6 +14,7 @@ export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND="bg=none,fg=white,bold"
 #eval $(thefuck --alias)
 export GEM_HOME="$HOME/gems"
 export XCURSOR_SIZE=24
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 
 export PATH="$HOME/.local/bin:$HOME/gems/bin:/usr/local/go/bin:$HOME/go/bin:/home/linuxbrew/.linuxbrew/bin:$PATH"
 export PATH="$HOME/.local/bin/DSC:$PATH"
