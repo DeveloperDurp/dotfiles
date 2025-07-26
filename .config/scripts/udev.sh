6af3f253d588d653490f1f2de2756632eebfc399
DeveloperDurp <DeveloperDurp@durp.info>
2025-07-26 07:15:16 -0500

    update

--- .config/scripts/udev.sh
+++ .config/scripts/udev.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/zsh
 
-performance() {
+#performance() {
+#
+#  echo '128' >/sys/class/leds/system76_acpi::kbd_backlight/brightness
+#  /usr/bin/system76-power profile performance
+#}
+#
+#balanced() {
+#
+#  echo '128' >/sys/class/leds/system76_acpi::kbd_backlight/brightness
+#  /usr/bin/system76-power profile balanced
+#}
+#
+#battery() {
+#
+#  echo '128' >/sys/class/leds/system76_acpi::kbd_backlight/brightness
+#  /usr/bin/system76-power profile battery
+#}
 
-  echo '128' >/sys/class/leds/system76_acpi::kbd_backlight/brightness
-  /usr/bin/system76-power profile performance
-}
+#case "$1" in
+#performance) performance ;;
+#balanced) balanced ;;
+#battery) battery ;;
+#*) echo "Please enter an action" ;;
+#esac
 
-battery() {
-
-  echo '128' >/sys/class/leds/system76_acpi::kbd_backlight/brightness
-  /usr/bin/system76-power profile battery
-}
-
-case "$1" in
-performance) performance ;;
-battery) battery ;;
-*) echo "Please enter an action" ;;
-esac
+echo '128' >/sys/class/leds/system76_acpi::kbd_backlight/brightness
+/usr/bin/system76-power profile $1
