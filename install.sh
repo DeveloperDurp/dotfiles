bbb509a6b18c8827a1c836abf033d591d26507d3
DeveloperDurp <DeveloperDurp@durp.info>
2025-09-27 07:31:01 -0500

    update

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ sudo apt update && sudo apt install stow
 stow --adopt .
 git reset --hard
 
+mkdir -m 0755 /nix && chown vscode /nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 nix-env -iA nixpkgs.myPackages
