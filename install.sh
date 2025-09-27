9ac3e09274ce009d1a4b6a56621bfdba60c7d8b7
DeveloperDurp <DeveloperDurp@durp.info>
2025-09-27 07:43:20 -0500

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 rm .bashrc .zshrc .ideavirmrc .gitmodules
-sudo apt update && sudo apt install stow
-stow --adopt .
-git reset --hard
+sudo apt update
 
 sudo mkdir -m 0755 /nix && sudo chown vscode /nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 /home/vscode/.nix-profile/bin/nix-env -iA nixpkgs.myPackages
+
+cd "/home/vscode/dotfiles"
+
+stow --adopt .
+git reset --hard
