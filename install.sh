c883a71794336856e09ee6e56403ef3942aff75e
DeveloperDurp <DeveloperDurp@durp.info>
2025-09-27 08:15:23 -0500

    update

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 rm .bashrc .zshrc .ideavirmrc .gitmodules
-sudo apt update && sudo apt install stow
-stow --adopt .
-git reset --hard
+sudo apt update && sudo apt install stow ansible -y
 
-sudo mkdir -m 0755 /nix && sudo chown vscode /nix
-sh <(curl -L https://nixos.org/nix/install) --no-daemon
-/home/vscode/.nix-profile/bin/nix-env -iA nixpkgs.myPackages
+make devpod
 
-cd "/home/vscode/dotfiles"
-
-stow --adopt .
-git reset --hard
+#stow --adopt .
+#git reset --hard
+#
+#sudo mkdir -m 0755 /nix && sudo chown vscode /nix
+#sh <(curl -L https://nixos.org/nix/install) --no-daemon
+#/home/vscode/.nix-profile/bin/nix-env -iA nixpkgs.myPackages
+#
+#cd "/home/vscode/dotfiles"
+#
+#stow --adopt .
+#git reset --hard
